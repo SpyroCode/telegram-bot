22292d1be7c0c0b40bbd4083514261dd314d4960
Efren Almanza Lamas <efren@intelligential.tech>
2022-09-03 19:12:49 -0500

    [ADD]ADD data configurations for database imports

---
+++ app/db/config/index.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const defaults = {
+    DB_DIALECT: 'postgres',
+    DB_HOST: 'localhost',
+    DB_PORT: '5432',
+    DEFAULT_TIMEZONE: 'UTC'
+}
+
+
+const dataConnection = Object.freeze({
+    dialect: process.env.DB_DIALECT || defaults.DB_DIALECT,
+    host: process.env.DB_HOST || defaults.DB_HOST,
+    port: process.env.DB_PORT || defaults.DB_PORT,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    logging: false,
+    timezone: defaults.DEFAULT_TIMEZONE
+})
+
+export default dataConnection
--- app/db/connection.ts
+++ app/db/connection.ts
@@ -1,9 +1,16 @@
 import { Sequelize } from 'sequelize'
+import dataConnection from "./config";
 
-const db = new Sequelize('challenge_bot', 'challenge-user', 'dOcFF29D4dxjyqF1BeGRA', {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: false
+const database: string = dataConnection.database || 'postgres'
+const dbUser: string = dataConnection.username || ''
+const dbPassword: string = dataConnection.password || ''
+const dbHost: string = dataConnection.host || 'localhost'
+const configDialect: string = dataConnection.dialect || ''
+const logging: boolean = dataConnection.logging || false
+const db = new Sequelize(database, dbUser, dbPassword, {
+    host: dbHost,
+    dialect: configDialect,
+    logging: logging
 })
 
 export default db;
