cbf0edef853e498cd090ef703e76f39215ac7396
Efren Almanza Lamas <efren@intelligential.tech>
2022-09-04 21:59:26 -0500

    [ADD]Add migration for scraping url

---
+++ app/db/migrations/20220905024603-scrap_url.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('scrap_url', {
+      id: {
+        type:Sequelize.DataTypes.UUID,
+        primaryKey:true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4
+      },
+      index: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false
+      },
+      url: Sequelize.DataTypes.STRING,
+      is_enabled: {
+        type: Sequelize.DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
+      },
+      is_active: {
+        type: Sequelize.DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DataTypes.DATE
+      },
+      updated_at: {
+        type: Sequelize.DataTypes.DATE
+      }
+    }, {schema: 'operations'});
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('scrap_url');
+  }
+};
