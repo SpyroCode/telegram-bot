94863df72ad6ecd52a98a09ee8f9770f35542a30
Efren Almanza Lamas <efren@intelligential.tech>
2022-09-05 23:04:55 -0500

    [ADD]ADD Validation save response update previo response

--- app/functions/product.ts
+++ app/functions/product.ts
@@ -2,18 +2,28 @@ import logger from "../logger";
 import {getUser} from "./user";
 import Product from "../db/models/product";
 import {scrapingProduct} from "../helpers/scraping";
-import { User } from "../interface/definitionTypes";
+import {ProductResult, User} from "../interface/definitionTypes";
+import {Model} from "sequelize";
 
 export const getProduct = async (data: any) => {
     const functionName = 'getProduct'
     try {
       logger.info(`Started function ${functionName}`)
       const user: User = await getUser(data)
+      const response: Array<ProductResult> = await scrapingProduct(data.formatMessageProduct)
+      const previoResponse: Model | null = await Product.findOne({
+          where: {
+              product: data.formatMessageProduct,
+              userId: user.id,
+              active: true
+          }
+      })
+      if (response && previoResponse)  await previoResponse.update({active: false})
       await Product.create({
           index: await generateProductIndex(user),
           product: data.formatMessageProduct,
           price: data.price,
-          response: await scrapingProduct(data.formatMessageProduct),
+          response,
           userId: user.id
       })
     } catch (err: any) {
